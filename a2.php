2662a9aef3d4173221b697349a8f09b07c548a8e
3A35 <91645564+3A35@users.noreply.github.com>
2023-02-12 21:41:48 +0800

    Add files via upload

---
+++ a2.php
@@ -0,0 +1,57 @@
+<HTML>
+<HEAD>
+<TITLE></TITLE>
+<SCRIPT Language="JavaScript">
+<!--
+function 小雯 ()
+{
+	alert("答對了");
+}
+function 阿琪 ()
+{
+	alert("答錯了");
+}
+function 子涵 ()
+{
+	alert("答錯了");
+	
+}
+
+function 小華 ()
+{
+	alert("答錯了");
+	
+}
+
+
+
+
+
+// -->
+</SCRIPT></HEAD>
+<BODY bgColor="#FFffFF">
+
+
+
+
+<div  style="font-size:100px" >誰最笨</div>
+
+<BR>
+
+
+
+
+<FORM>
+<INPUT TYPE="button" VALUE="小雯" style="width:160px;height:100px;font-size:70px;" onClick="小雯()">
+<INPUT TYPE="button" VALUE="阿琪" style="width:160px;height:100px;font-size:70px;" onClick="阿琪()">
+<INPUT TYPE="button" VALUE="子涵" style="width:160px;height:100px;font-size:70px;" onClick="子涵()">
+<INPUT TYPE="button" VALUE="小華" style="width:160px;height:100px;font-size:70px;" onClick="小華()">
+
+</FORM>
+
+<?php
+echo("hello");
+?>
+
+</BODY>
+</HTML>
